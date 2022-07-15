c8c4e7fe101dcff64556ab7ca183adb7fb52d9c9
vis4rd <alagaesia.vrael@gmail.com>
2022-07-15 18:27:16 +0200

    Prepare Window class for Updateable trait objects

--- include/window/Window.hpp
+++ include/window/Window.hpp
@@ -12,24 +12,30 @@ class Window final
 
     GLFWwindow *getWindow();
     const ImGuiIO &getImGuiIo() const;
-    glm::uvec2 getSize() const;
+    std::pair<int32_t, int32_t> getSize() const;
     const std::string &getTitle() const;
 
     bool isMaximized() const;
     bool isVerticalSyncEnabled() const;
 
+    void setSize(const std::pair<int32_t, int32_t> &new_size);
     void setTitle(const std::string &title);
     void setFullscreen(bool fullscreen = true);
     void setMaximized(bool maximized = true);
     void setVerticalSync(bool vsync = true);
 
-    bool update() noexcept;
+    template<typename... UPDATEABLES>
+    requires CUpdateablePack<UPDATEABLES...>
+    bool update(UPDATEABLES &&...updateables) noexcept;
 
     template<typename... RENDERABLES>
     requires CRenderablePack<RENDERABLES...>
     void render(RENDERABLES &&...renderables) noexcept;
 
     private:
+    void initGLFW();
+    void initImGui();
+    void initGL();
     void terminate();
 
     private:
@@ -38,8 +44,48 @@ class Window final
     std::string m_title{};
     uint32_t m_width = 0;
     uint32_t m_height = 0;
+    bool m_shouldClose = false;
+    SectionManager &m_sectionManager = SectionManager::get();
+    Input &m_inputManager = Input::get();
 };
 
+
+template<typename... UPDATEABLES>
+requires CUpdateablePack<UPDATEABLES...>
+bool Window::update(UPDATEABLES &&...updateables) noexcept
+{
+    spdlog::trace("Polling GLFW events");
+    glfwPollEvents();
+
+    spdlog::trace("Updating updateables passed to Window::update()");
+    if constexpr(sizeof...(updateables) > 0)
+    {
+        (updateables.update(), ...);
+    }
+
+    if(glfwWindowShouldClose(m_window))
+    {
+        spdlog::debug("GLFW asks to close the window");
+        m_shouldClose = true;
+    }
+    if(m_sectionManager.size() < 0)
+    {
+        spdlog::debug("SectionManager asks to close the window");
+        m_shouldClose = true;
+    }
+    else
+    {
+        m_sectionManager.clear();
+    }
+
+    if(m_shouldClose)
+    {
+        this->terminate();
+    }
+
+    spdlog::trace("Window::update() returns {}", !m_shouldClose);
+    return !m_shouldClose;
+}
 
 template<typename... RENDERABLES>
 requires CRenderablePack<RENDERABLES...>
--- src/window/Window.cpp
+++ src/window/Window.cpp
@@ -3,55 +3,10 @@
 Window::Window(const std::string &window_title, uint32_t width, uint32_t height)
     : m_title(window_title), m_width(width), m_height(height)
 {
-    if(!glfwInit())
-    {
-        throw std::runtime_error("Failed to initialize GLFW.");
-    }
+    this->initGLFW();
+    this->initImGui();
+    this->initGL();
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
-    glfwWindowHint(GLFW_DOUBLEBUFFER, GLFW_TRUE);
-    glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
-
-    m_window = glfwCreateWindow(m_width, m_height, m_title.c_str(), nullptr, nullptr);
-    if(!m_window)
-    {
-        std::cout << "Failed to create GLFW window." << std::endl;
-        this->terminate();
-    }
-
-    glfwMakeContextCurrent(m_window);
-    this->setVerticalSync(true);
-
-    // Setup Dear ImGui context
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    auto &io = ImGui::GetIO();
-    (void)io;
-    // io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
-    // io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad; // Enable Gamepad Controls
-    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;  // Enable Docking
-    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;  // Enable Multi-Viewport
-    // io.ConfigViewportsNoAutoMerge = true;
-    // io.ConfigViewportsNoTaskBarIcon = true;
-    m_io = io;
-
-    // Setup Dear ImGui style
-    ImGui::StyleColorsDark();
-
-    // Setup Platform/Renderer backends
-    ImGui_ImplGlfw_InitForOpenGL(m_window, true);
-    ImGui_ImplOpenGL3_Init("#version 450");
-
-    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD." << std::endl;
-        this->terminate();
-    }
-
-    glViewport(0, 0, width, height);
     // glfwSetWindowSizeCallback(m_window,
     //     [](GLFWwindow *window, int new_width, int new_height) -> void
     //     {
@@ -83,11 +38,11 @@ const ImGuiIO &Window::getImGuiIo() const
     return m_io;
 }
 
-glm::uvec2 Window::getSize() const
+std::pair<int32_t, int32_t> Window::getSize() const
 {
     int32_t width, height;
     glfwGetWindowSize(m_window, &width, &height);
-    return glm::uvec2(width, height);
+    return {width, height};
 }
 
 const std::string &Window::getTitle() const
@@ -104,6 +59,11 @@ bool Window::isVerticalSyncEnabled() const
 {
     std::cout << "'bool Window::isVerticalSyncEnabled() const' is not currently implemented." << std::endl;
     return false;
+}
+
+void Window::setSize(const std::pair<int32_t, int32_t> &new_size)
+{
+    glfwSetWindowSize(m_window, new_size.first, new_size.second);
 }
 
 void Window::setTitle(const std::string &title)
@@ -141,19 +101,10 @@ void Window::setVerticalSync(bool vsync)
     glfwSwapInterval(static_cast<int>(vsync));
 }
 
-bool Window::update() noexcept
-{
-    if(glfwWindowShouldClose(m_window))
-    {
-        return false;
-    }
-
-    glfwPollEvents();
-    return true;
-}
-
 void Window::terminate()
 {
+    spdlog::info("Terminating window instance");
+
     ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplGlfw_Shutdown();
     ImGui::DestroyContext();
@@ -164,4 +115,70 @@ void Window::terminate()
         m_window = nullptr;
     }
     glfwTerminate();
+}
+
+void Window::initGLFW()
+{
+    spdlog::debug("Initializing GLFW");
+
+    if(!glfwInit())
+    {
+        spdlog::critical("Failed to initialize GLFW library");
+        throw std::runtime_error("Failed to initialize GLFW library.");
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+    glfwWindowHint(GLFW_DOUBLEBUFFER, GLFW_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+
+    m_window = glfwCreateWindow(m_width, m_height, m_title.c_str(), nullptr, nullptr);
+    if(!m_window)
+    {
+        spdlog::critical("Failed to initialize GLFW window");
+        this->terminate();
+    }
+
+    glfwMakeContextCurrent(m_window);
+    this->setVerticalSync(true);
+}
+
+void Window::initImGui()
+{
+    spdlog::debug("Initializing ImGui");
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    auto &io = ImGui::GetIO();
+    (void)io;
+    // io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
+    // io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad; // Enable Gamepad Controls
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;  // Enable Docking
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;  // Enable Multi-Viewport
+    // io.ConfigViewportsNoAutoMerge = true;
+    // io.ConfigViewportsNoTaskBarIcon = true;
+    m_io = io;
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(m_window, true);
+    ImGui_ImplOpenGL3_Init("#version 450");
+}
+
+void Window::initGL()
+{
+    spdlog::debug("Initializing GLAD");
+
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        spdlog::critical("Failed to initialize GLAD");
+        this->terminate();
+    }
+
+    glViewport(0, 0, m_width, m_height);
 }
