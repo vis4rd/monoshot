vis4rd/monoshot

commit c8657462600e3624c8d5363cde6ea04f9a6b80a7

Improve Input class implementation

include/input/Input.hpp
@@ -13,8 +13,10 @@ class Input
     };
     struct Group
     {
-        std::string name;
-        std::unordered_map<int32_t, Keybind> keybinds;
+        Group() = default;
+        Group(const std::string& name) : name(name) { }
+        std::string name{};
+        std::unordered_map<int32_t, Keybind> keybinds{};
     };
 
     public:
@@ -61,11 +63,11 @@ constexpr void Input::addKeybind(const std::string& group,
         throw std::runtime_error("Given keybind group does not exist.");
     }
 
-    auto wrapper = [key, action, callback, cb_args...](GLFWwindow* window)
+    auto wrapper = [&key, &action, &callback, &cb_args...](GLFWwindow* window)
     {
         if(glfwGetKey(window, key) == action)
         {
-            std::invoke(callback, cb_args...);
+            std::invoke(std::forward<FUNC>(callback), std::forward<Args>(cb_args)...);
         }
     };
     iter->keybinds[key] = Keybind{key, glfwGetKeyScancode(key), action, std::move(wrapper)};

src/input/Input.cpp
@@ -8,17 +8,13 @@ Input& Input::get()
 
 const std::size_t Input::addGroup(const std::string& name)
 {
-    Group new_group;
-    if(name.empty())
+    auto valid_name(name);
+    if(valid_name.empty())
     {
-        new_group.name = "Keybind Group No. " + std::to_string(m_keybinds.size());
+        valid_name = "Keybind Group No. " + std::to_string(m_keybinds.size());
     }
-    else
-    {
-        new_group.name = name;
-    }
-    m_keybinds.push_back(new_group);
-    // std::cout << m_keybinds[0].name << std::endl;
+    m_keybinds.emplace_back(std::move(valid_name));
+    spdlog::info("Creating a new keybind group called '{}'", m_keybinds[m_keybinds.size() - 1].name);
     return m_keybinds.size() - 1;
 }
 

commit ff347804644661d0ccb256f54c633e9c70af18f2

Fix a bug introduced in c8657462600e3624c8d5363cde6ea04f9a6b80a7, revert to previous implementation

include/input/Input.hpp
@@ -9,7 +9,7 @@ class Input
         int32_t key;
         int32_t scancode;
         int32_t action;
-        std::function<void(GLFWwindow*)> callback;
+        std::function<void(GLFWwindow*, int32_t, int32_t)> callback;
     };
     struct Group
     {
@@ -52,6 +52,7 @@ constexpr void Input::addKeybind(const std::string& group,
     FUNC&& callback,
     Args&&... cb_args)
 {
+    spdlog::info("Adding new keybind to group {}", group);
     auto iter = std::find_if(m_keybinds.begin(),
         m_keybinds.end(),
         [&group](const Group& g)
@@ -63,11 +64,11 @@ constexpr void Input::addKeybind(const std::string& group,
         throw std::runtime_error("Given keybind group does not exist.");
     }
 
-    auto wrapper = [&key, &action, &callback, &cb_args...](GLFWwindow* window)
+    auto wrapper = [callback, cb_args...](GLFWwindow* window, int32_t key, int32_t action)
     {
         if(glfwGetKey(window, key) == action)
         {
-            std::invoke(std::forward<FUNC>(callback), std::forward<Args>(cb_args)...);
+            std::invoke(callback, cb_args...);
         }
     };
     iter->keybinds[key] = Keybind{key, glfwGetKeyScancode(key), action, std::move(wrapper)};

src/input/Input.cpp
@@ -20,6 +20,7 @@ const std::size_t Input::addGroup(const std::string& name)
 
 void Input::processGroup(GLFWwindow* window, const std::string& group)
 {
+    spdlog::trace("Processing keybind group '{}'", group);
     auto iter = std::find_if(m_keybinds.begin(),
         m_keybinds.end(),
         [&group](const Group& g)
@@ -30,7 +31,7 @@ void Input::processGroup(GLFWwindow* window, const std::string& group)
     {
         for(auto& [glfw_key, keybind] : iter->keybinds)
         {
-            keybind.callback(window);
+            keybind.callback(window, keybind.key, keybind.action);
         }
     }
     else