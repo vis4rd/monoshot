117b845d3af47a1947125307ef24fa2812e07d70
vis4rd <alagaesia.vrael@gmail.com>
2022-07-24 18:09:07 +0200

    Add convenience functions for printing glm::mat4 and glm::vec3

--- include/shader/Shader.hpp
+++ include/shader/Shader.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "../Root.hpp"
+#include "../utility/Printing.hpp"
 
 namespace fs = std::filesystem;
 
---
+++ include/utility/Printing.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "../Root.hpp"
+
+namespace util
+{
+
+inline auto mat4str(const glm::mat4& mat)
+{
+    return fmt::format("{}, {}, {}, {}\n{}, {}, {}, {}\n{}, {}, {}, {}\n{}, {}, {}, {}",
+        mat[0][0],
+        mat[1][0],
+        mat[2][0],
+        mat[3][0],
+        mat[0][1],
+        mat[1][1],
+        mat[2][1],
+        mat[3][1],
+        mat[0][2],
+        mat[1][2],
+        mat[2][2],
+        mat[3][2],
+        mat[0][3],
+        mat[1][3],
+        mat[2][3],
+        mat[3][3]);
+}
+
+inline auto vec3str(const glm::vec3& vec)
+{
+    return fmt::format("({}, {}, {})", vec[0], vec[1], vec[2]);
+}
+
+}  // namespace util
