36fe3133bc2d6e7e10607617495197e4414282a6
vis4rd <alagaesia.vrael@gmail.com>
2022-08-16 23:06:18 +0200

    Create a global ResourceManager struct

---
+++ include/utility/ResourceManager.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "../window/Window.hpp"
+
+struct ResourceManager
+{
+    static std::shared_ptr<Window> window;
+};
---
+++ src/utility/ResourceManager.cpp
@@ -0,0 +1,3 @@
+#include "../../include/utility/ResourceManager.hpp"
+
+std::shared_ptr<Window> ResourceManager::window;
