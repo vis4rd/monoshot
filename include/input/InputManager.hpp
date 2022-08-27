595356becb8e548cb286359daab3ab90dab11c6c
vis4rd <alagaesia.vrael@gmail.com>
2022-08-28 00:19:39 +0200

    Add management of mouse events in InputManager

--- include/input/InputManager.hpp
+++ include/input/InputManager.hpp
@@ -14,23 +14,19 @@ class InputManager
     const std::size_t addGroup(const std::string& name = {});
 
     template<typename FUNC, typename... Args>
-    constexpr void addKeybind(const std::string& group,
-        int32_t key,
-        KeyState state,
-        FUNC&& callback,
-        Args&&... cb_args);
+    constexpr void addKeybind(const std::string& group, int32_t key, KeyState state, FUNC&& callback, Args&&... cb_args);
     template<typename FUNC, typename... Args>
-    constexpr void addKeybind(const std::size_t& group_id,
-        int32_t key,
-        KeyState state,
-        FUNC&& callback,
-        Args&&... cb_args);
+    constexpr void addKeybind(const std::size_t& group_id, int32_t key, KeyState state, FUNC&& callback, Args&&... cb_args);
     void processGroup(GLFWwindow* window, const std::string& group);
     void removeGroup(const std::string& group);
     void removeKeybind(const std::string& group, int32_t glfw_key);
 
     private:
     InputManager() = default;
+    bool is_valid_key(const std::int32_t key) const;
+    bool is_printable_key(const std::int32_t key) const;
+    bool is_functional_key(const std::int32_t key) const;
+    bool is_mouse_key(const std::int32_t key) const;
 
     private:
     std::vector<KeyGroup> m_keybinds;
@@ -39,12 +35,13 @@ class InputManager
 };
 
 template<typename FUNC, typename... Args>
-constexpr void InputManager::addKeybind(const std::string& group,
-    int32_t key,
-    KeyState state,
-    FUNC&& callback,
-    Args&&... cb_args)
+constexpr void InputManager::addKeybind(const std::string& group, int32_t key, KeyState state, FUNC&& callback, Args&&... cb_args)
 {
+    if(!this->is_valid_key(key))
+    {
+        spdlog::warn("Given key ID '{}' is not valid, ignoring...", key);
+        return;
+    }
     auto iter = std::find_if(m_keybinds.begin(),
         m_keybinds.end(),
         [&group](const KeyGroup& g)
@@ -66,17 +63,16 @@ constexpr void InputManager::addKeybind(const std::string& group,
 }
 
 template<typename FUNC, typename... Args>
-constexpr void InputManager::addKeybind(const std::size_t& group_id,
-    int32_t key,
-    KeyState state,
-    FUNC&& callback,
-    Args&&... cb_args)
+constexpr void InputManager::addKeybind(const std::size_t& group_id, int32_t key, KeyState state, FUNC&& callback, Args&&... cb_args)
 {
+    if(!this->is_valid_key(key))
+    {
+        spdlog::warn("Given key ID '{}' is not valid, ignoring...", key);
+        return;
+    }
     if((group_id >= m_keybinds.size()) || (group_id < 0))
     {
-        spdlog::debug("Given group_id = {} is out of bounds in keybinds vector of size {}",
-            group_id,
-            m_keybinds.size());
+        spdlog::debug("Given group_id = {} is out of bounds in keybinds vector of size {}", group_id, m_keybinds.size());
         spdlog::critical("Given keybind group does not exist");
         throw std::runtime_error("Given keybind group does not exist.");
     }
--- src/input/Keybind.cpp
+++ src/input/Keybind.cpp
@@ -1,6 +1,9 @@
 #include "../../include/input/Keybind.hpp"
 
 Keybind::Keybind(int32_t key, int32_t scancode, KeyState state, std::function<void()> callback)
-    : key(key), scancode(scancode), state(state), callback(callback)
+    : key(key),
+      scancode(scancode),
+      state(state),
+      callback(callback)
 {
 }
