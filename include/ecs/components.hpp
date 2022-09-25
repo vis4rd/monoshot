8d25999a592eb7e67029462c8063601dc7624e7d
vis4rd <alagaesia.vrael@gmail.com>
2022-09-26 01:33:00 +0200

    🎨 Extend Scalar template class functionality, change formatting of components

--- include/ecs/components.hpp
+++ include/ecs/components.hpp
@@ -9,49 +9,59 @@ namespace ecs::component
 namespace impl
 {
     template <typename T>
-    class ImplicitConversionsInterface
+    class Scalar
     {
         public:
-        ImplicitConversionsInterface(const T& val) : data(val) { }
-        ImplicitConversionsInterface() : ImplicitConversionsInterface(static_cast<T>(0)) { }
-        operator T&() { return data; }
-        operator const T&() const { return data; }
-        operator T() const { return data; }
+        Scalar(const T& val) : data(std::move(val))         { }
+        
+        Scalar() : data(static_cast<T>(0))                  { }
+        Scalar(const Scalar& copy) : data(copy.data)        { }
+        Scalar(Scalar&& move) : data(std::move(move.data))  { }
+
+        operator T&()               { return data; }
+        operator const T&() const   { return data; }
+        operator T() const          { return data; }
+
+        Scalar& operator=(const Scalar& copy)   { this->data = copy.data; return *this; }
+        Scalar& operator=(Scalar&& move)        { this->data = std::move(data); return *this; }
+        Scalar& operator=(const T& val)         { this->data = std::move(val); return *this; }
+
+        Scalar operator+(const Scalar& rhs) const { Scalar retval; retval.data = this->data + rhs.data; return retval; }
+        Scalar operator-(const Scalar& rhs) const { Scalar retval; retval.data = this->data - rhs.data; return retval; }
+        Scalar operator/(const Scalar& rhs) const { Scalar retval; retval.data = this->data / rhs.data; return retval; }
+        Scalar operator*(const Scalar& rhs) const { Scalar retval; retval.data = this->data * rhs.data; return retval; }
+        Scalar operator+(const T& rhs)      const { Scalar retval; retval.data = this->data + rhs; return retval; }
+        Scalar operator-(const T& rhs)      const { Scalar retval; retval.data = this->data - rhs; return retval; }
+        Scalar operator/(const T& rhs)      const { Scalar retval; retval.data = this->data / rhs; return retval; }
+        Scalar operator*(const T& rhs)      const { Scalar retval; retval.data = this->data * rhs; return retval; }
+
+        Scalar& operator+=(const Scalar& rhs)   { this->data += rhs.data; return *this; }
+        Scalar& operator-=(const Scalar& rhs)   { this->data -= rhs.data; return *this; }
+        Scalar& operator/=(const Scalar& rhs)   { this->data /= rhs.data; return *this; }
+        Scalar& operator*=(const Scalar& rhs)   { this->data *= rhs.data; return *this; }
+        Scalar& operator+=(const T& rhs)        { this->data += rhs; return *this; }
+        Scalar& operator-=(const T& rhs)        { this->data -= rhs; return *this; }
+        Scalar& operator/=(const T& rhs)        { this->data /= rhs; return *this; }
+        Scalar& operator*=(const T& rhs)        { this->data *= rhs; return *this; }
 
         public:
         T data;
     };
-    // TODO: replace this with 'scalar' base class with all operators overloaded
 }
 
-struct position : public glm::vec2
-{
+struct position : public glm::vec2 { };
+struct rotation : public impl::Scalar<float> { };
+struct velocity : public impl::Scalar<float> { };
+struct direction : public glm::vec2 { };  // direction of movement in degrees 
 
+struct max_velocity : public impl::Scalar<float>
+{
+    max_velocity() : impl::Scalar<float>(7.f) { }
 };
 
-struct rotation : public impl::ImplicitConversionsInterface<float>
+struct acceleration : public impl::Scalar<float>
 {
-
-};
-
-struct velocity : public impl::ImplicitConversionsInterface<float>
-{
-
-};
-
-struct max_velocity : public impl::ImplicitConversionsInterface<float>
-{
-    max_velocity() : impl::ImplicitConversionsInterface<float>(7.f) { }
-};
-
-struct acceleration : public impl::ImplicitConversionsInterface<float>
-{
-    acceleration() : impl::ImplicitConversionsInterface<float>(100.f) { }
-};
-
-struct direction : public impl::ImplicitConversionsInterface<float> // direction of movement in degrees
-{
-
+    acceleration() : impl::Scalar<float>(100.f) { }
 };
 
 // clang-format on
