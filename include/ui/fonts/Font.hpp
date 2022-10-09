2b726ed82d6bd89b5140933aef722af719a97aee
vis4rd <alagaesia.vrael@gmail.com>
2022-10-09 02:19:40 +0200

    💩 Add a very simple Font abstraction (this level of laziness should be illegal)

---
+++ include/ui/fonts/Font.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <string_view>
+
+class ImFont;
+
+namespace impl
+{
+
+class FontUsageGuard
+{
+    public:
+    FontUsageGuard(ImFont* font);
+    ~FontUsageGuard();
+
+    void popFont();
+
+    private:
+    std::uint32_t m_pushCount = 0;
+};
+
+}  // namespace impl
+
+class Font
+{
+    public:
+    Font(std::string_view file_path, float size = 16.f);
+    ImFont* loadFromFile(std::string_view file_path, float size = 16.f);
+    // clang-format off
+    [[nodiscard("This function returns a RAII guard which pushes and pops the ImGui fonts for you")]]
+    impl::FontUsageGuard use(ImFont* font = nullptr) const;
+    ImFont* get();
+    // clang-format on
+
+    private:
+    ImFont* m_font = nullptr;
+};
