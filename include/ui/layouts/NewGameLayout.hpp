14646f0b3b4700f687cb4c9b377f9db92d7cf059
vis4rd <alagaesia.vrael@gmail.com>
2022-10-15 16:48:19 +0200

    💄 Create NewGameLayout

---
+++ include/ui/layouts/NewGameLayout.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "BaseLayout.hpp"
+
+namespace UI
+{
+
+struct NewGameLayout : public BaseLayout
+{
+    inline NewGameLayout(const ImVec2& workpos, const ImVec2& worksize);
+    inline void update(const ImVec2& workpos, const ImVec2& worksize) override;
+};
+
+NewGameLayout::NewGameLayout(const ImVec2& workpos, const ImVec2& worksize)
+    : BaseLayout(workpos, worksize)
+{
+    base_menu_w = 1840.f;
+    base_menu_h = 512.f;
+    base_button_w = 512.f;
+    this->update(workpos, worksize);
+    // menu_y = viewport_y + (viewport_h - menu_h) / 2.f;
+}
+
+void NewGameLayout::update(const ImVec2& workpos, const ImVec2& worksize)
+{
+    this->BaseLayout::update(workpos, worksize);
+    menu_y = viewport_y + (viewport_h - menu_h) / 2.f;
+}
+
+}  // namespace UI
