b353cc15ae8c4612596bfe85a0679dbfc8cc6297
vis4rd <alagaesia.vrael@gmail.com>
2022-10-15 16:50:20 +0200

    💄 Fix spacing of MainMenuSection buttons and make NewGame button point to NewGameSection

--- src/section/MainMenuSection.cpp
+++ src/section/MainMenuSection.cpp
@@ -19,7 +19,10 @@ MainMenuSection::MainMenuSection()
         });
 }
 
-void MainMenuSection::update() noexcept { }
+void MainMenuSection::update() noexcept
+{
+    m_layout.update(ImGui::GetMainViewport()->WorkPos, ImGui::GetMainViewport()->WorkSize);
+}
 
 void MainMenuSection::render() noexcept
 {
@@ -27,13 +30,13 @@ void MainMenuSection::render() noexcept
     const auto& title_font = res::uiTitleFont;
     const auto& title_font_size = *res::uiTitleFontSize;
 
-    m_layout.update(ImGui::GetMainViewport()->WorkPos, ImGui::GetMainViewport()->WorkSize);
-
     auto title_font_guard = title_font->use();
     ImGui::SetNextWindowPos({m_layout.menu_x, m_layout.menu_y - title_font_size - 100.f});
     ImGui::SetNextWindowSize({ResourceManager::window->getSize().x - 200.f, title_font->get()->FontSize + 50.f});
     ImGui::Begin("MainMenuGameTitle", nullptr, m_layout.window_flags);
     {
+        auto next_y = ImGui::GetCursorScreenPos().y;
+        ImGui::SetCursorScreenPos({m_layout.menu_x + m_layout.button_w_s, next_y});
         ImGui::Text("%s", m_titleText.c_str());
     }
     ImGui::End();
@@ -47,25 +50,29 @@ void MainMenuSection::render() noexcept
     ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, {m_layout.button_w_s, m_layout.button_h_s});
     ImGui::Begin("MainMenu", nullptr, m_layout.window_flags);
     {
+        ImGui::SetCursorScreenPos({m_layout.menu_x + m_layout.button_w_s, m_layout.menu_y + m_layout.button_h_s});
         if(ImGui::Button("New game", {m_layout.button_w, m_layout.button_h}))
         {
             spdlog::debug("Clicking 'New game'");
             auto& sm = SectionManager::get();
-            sm.emplaceSection<DebugSection>();
+            sm.emplaceSection<NewGameSection>();
         }
-        else if(ImGui::Button("Map creator", {m_layout.button_w, m_layout.button_h}))
+        ImGui::SetCursorScreenPos({m_layout.menu_x + m_layout.button_w_s, ImGui::GetCursorScreenPos().y});
+        if(ImGui::Button("Map creator", {m_layout.button_w, m_layout.button_h}))
         {
             spdlog::debug("Clicking 'Map creator'");
             auto& sm = SectionManager::get();
             sm.emplaceSection<CreatorSection>();
         }
-        else if(ImGui::Button("Settings", {m_layout.button_w, m_layout.button_h}))
+        ImGui::SetCursorScreenPos({m_layout.menu_x + m_layout.button_w_s, ImGui::GetCursorScreenPos().y});
+        if(ImGui::Button("Settings", {m_layout.button_w, m_layout.button_h}))
         {
             spdlog::debug("Clicking 'Settings'");
             auto& sm = SectionManager::get();
             sm.emplaceSection<SettingsSection>();
         }
-        else if(ImGui::Button("Exit", {m_layout.button_w, m_layout.button_h}))
+        ImGui::SetCursorScreenPos({m_layout.menu_x + m_layout.button_w_s, ImGui::GetCursorScreenPos().y});
+        if(ImGui::Button("Exit", {m_layout.button_w, m_layout.button_h}))
         {
             spdlog::debug("Clicking 'Exit'");
             SectionManager::get().clear();
