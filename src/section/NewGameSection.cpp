6ef51c00bbd898798124e07388fde461646e6a83
vis4rd <alagaesia.vrael@gmail.com>
2022-10-15 16:49:43 +0200

    💄 Setup NewGameSection and for now add DebugSection as the only option

--- src/section/NewGameSection.cpp
+++ src/section/NewGameSection.cpp
@@ -1,19 +1,65 @@
 #include "../../include/section/NewGameSection.hpp"
 
-NewGameSection::NewGameSection() : Section()
+#include "../../include/utility/ResourceManager.hpp"
+
+NewGameSection::NewGameSection()
+    : Section(),
+      m_layout(ImGui::GetMainViewport()->WorkPos, ImGui::GetMainViewport()->WorkSize),
+      m_navLayout(ImGui::GetMainViewport()->WorkPos, ImGui::GetMainViewport()->WorkSize)
 {
     m_name = "NewGameSection";
-    auto& input_manager = InputManager::get();
-    auto group_id = input_manager.addGroup(m_name);
-    input_manager.addKeybind(group_id,
-        GLFW_KEY_ESCAPE,
-        KeyState::PRESS_ONCE,
-        []
-        {
-            SectionManager::get().clear();
-        });
 }
 
-void NewGameSection::update() noexcept { }
+void NewGameSection::update() noexcept
+{
+    const ImGuiViewport& main_viewport = *ImGui::GetMainViewport();
+    m_layout.update(main_viewport.WorkPos, main_viewport.WorkSize);
+    m_navLayout.update(main_viewport.WorkPos, main_viewport.WorkSize);
 
-void NewGameSection::render() noexcept { }
+    auto& input = InputManager::get();
+    if(input.isPressedOnce(GLFW_KEY_ESCAPE))
+    {
+        SectionManager::get().popSection();
+    }
+}
+
+void NewGameSection::render() noexcept
+{
+    using res = ResourceManager;
+
+    const auto& title_font = res::uiTitleFont;
+    const auto& title_font_size = *res::uiTitleFontSize;
+
+    auto title_font_guard = title_font->use();
+    ImGui::SetNextWindowPos({m_layout.menu_x, m_layout.menu_y - title_font_size - 100.f});
+    ImGui::SetNextWindowSize({ResourceManager::window->getSize().x - 200.f, title_font->get()->FontSize + 50.f});
+    ImGui::Begin("NewGameMenuGameTitle", nullptr, m_layout.window_flags);
+    {
+        auto next_y = ImGui::GetCursorScreenPos().y;
+        ImGui::SetCursorScreenPos({m_layout.menu_x + m_layout.button_w_s, next_y});
+        ImGui::Text("MONOSHOT");
+    }
+    ImGui::End();
+    title_font_guard.popFont();
+
+    const auto& button_font = res::uiButtonFont;
+
+    auto button_font_guard = button_font->use();
+    ImGui::SetNextWindowPos({m_layout.menu_x, m_layout.menu_y});
+    ImGui::SetNextWindowSize({m_layout.menu_w, m_layout.menu_h});
+    ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, {m_layout.button_w_s, m_layout.button_h_s});
+    ImGui::Begin("NewGameMenu", nullptr, m_layout.window_flags);
+    {
+        ImGui::SetCursorScreenPos({m_layout.menu_x + m_layout.button_w_s, m_layout.menu_y + m_layout.button_h_s});
+        if(ImGui::Button("Debug Level", {m_layout.button_w, m_layout.button_h}))
+        {
+            spdlog::debug("Clicking 'Debug Level'");
+            auto& sm = SectionManager::get();
+            sm.emplaceSection<DebugSection>();
+        }
+    }
+    ImGui::End();
+    ImGui::PopStyleVar();
+
+    UI::LowerNavigationBox(m_navLayout);
+}
