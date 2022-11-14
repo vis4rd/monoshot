6701e3856ff2a5f9f7928676316e4144d025f654
vis4rd <alagaesia.vrael@gmail.com>
2022-11-14 13:51:37 +0100

    🎨 Extract Scalar class to a seprate file in traits/

--- include/ecs/components.hpp
+++ include/ecs/components.hpp
@@ -3,63 +3,20 @@
 #include <glm/glm.hpp>
 
 #include "../utility/ResourceManager.hpp"
+#include "../traits/Scalar.hpp"
 
 namespace ecs::component
 {
 // clang-format off
 
-namespace impl
-{
-    template <typename T>
-    class Scalar
-    {
-        public:
-        Scalar(const T& val) : data(val)         { }
-        
-        Scalar() : data(static_cast<T>(0))                  { }
-        Scalar(const Scalar& copy) : data(copy.data)        { }
-        Scalar(Scalar&& move) : data(std::move(move.data))  { }
-        ~Scalar() = default;
-
-        operator T&()               { return data; }
-        operator const T&() const   { return data; }
-        operator T() const          { return data; }
-
-        Scalar& operator=(const Scalar& copy)   { this->data = copy.data; return *this; }
-        Scalar& operator=(Scalar&& move)        { this->data = std::move(move.data); return *this; }
-        Scalar& operator=(const T& val)         { this->data = std::move(val); return *this; }
-
-        Scalar operator+(const Scalar& rhs) const { Scalar retval; retval.data = this->data + rhs.data; return retval; }
-        Scalar operator-(const Scalar& rhs) const { Scalar retval; retval.data = this->data - rhs.data; return retval; }
-        Scalar operator/(const Scalar& rhs) const { Scalar retval; retval.data = this->data / rhs.data; return retval; }
-        Scalar operator*(const Scalar& rhs) const { Scalar retval; retval.data = this->data * rhs.data; return retval; }
-        Scalar operator+(const T& rhs)      const { Scalar retval; retval.data = this->data + rhs; return retval; }
-        Scalar operator-(const T& rhs)      const { Scalar retval; retval.data = this->data - rhs; return retval; }
-        Scalar operator/(const T& rhs)      const { Scalar retval; retval.data = this->data / rhs; return retval; }
-        Scalar operator*(const T& rhs)      const { Scalar retval; retval.data = this->data * rhs; return retval; }
-
-        Scalar& operator+=(const Scalar& rhs)   { this->data += rhs.data; return *this; }
-        Scalar& operator-=(const Scalar& rhs)   { this->data -= rhs.data; return *this; }
-        Scalar& operator/=(const Scalar& rhs)   { this->data /= rhs.data; return *this; }
-        Scalar& operator*=(const Scalar& rhs)   { this->data *= rhs.data; return *this; }
-        Scalar& operator+=(const T& rhs)        { this->data += rhs; return *this; }
-        Scalar& operator-=(const T& rhs)        { this->data -= rhs; return *this; }
-        Scalar& operator/=(const T& rhs)        { this->data /= rhs; return *this; }
-        Scalar& operator*=(const T& rhs)        { this->data *= rhs; return *this; }
-
-        public:
-        T data;
-    };
-}
-
 struct size : public glm::vec2 { };
 struct position : public glm::vec2 { };
-struct rotation : public impl::Scalar<float> { };
-struct velocity : public impl::Scalar<float> { };
+struct rotation : public Scalar<float> { };
+struct velocity : public Scalar<float> { };
 struct direction : public glm::vec2 { };  // direction of movement in degrees
-struct collision : public impl::Scalar<bool> { };
-struct max_velocity : public impl::Scalar<float> { max_velocity(float val = 7.f) : impl::Scalar<float>(val) { } };
-struct acceleration : public impl::Scalar<float> { acceleration(float val = 100.f) : impl::Scalar<float>(val) { } };
+struct collision : public Scalar<bool> { };
+struct max_velocity : public Scalar<float> { max_velocity(float val = 7.f) : Scalar<float>(val) { } };
+struct acceleration : public Scalar<float> { acceleration(float val = 100.f) : Scalar<float>(val) { } };
 struct is_bullet{};
 struct lifetime
 {
---
+++ include/traits/Scalar.hpp
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <utility>
+#include <concepts>
+
+template<typename T>
+concept NumericC = std::integral<T> or std::floating_point<T>;
+
+// clang-format off
+template<NumericC T>
+class Scalar
+{
+    public:
+    constexpr Scalar(const T& val) : data(val)       { }
+    constexpr Scalar(T&& val) : data(std::move(val)) { }
+
+    constexpr Scalar() : data(static_cast<T>(0))                 { }
+    constexpr Scalar(const Scalar& copy) : data(copy.data)       { }
+    constexpr Scalar(Scalar&& move) : data(std::move(move.data)) { }
+    constexpr ~Scalar() = default;
+
+    operator T&()             { return data; }
+    operator const T&() const { return data; }
+    operator T()        const { return data; }
+
+    constexpr Scalar& operator=(const Scalar& copy) { this->data = copy.data; return *this; }
+    constexpr Scalar& operator=(Scalar&& move)      { this->data = std::move(move.data); return *this; }
+    constexpr Scalar& operator=(const T& val)       { this->data = std::move(val); return *this; }
+    constexpr Scalar& operator=(T&& val)            { this->data = std::move(val); return *this; }
+
+    constexpr Scalar operator+(const Scalar& rhs) const { Scalar retval; retval.data = this->data + rhs.data; return retval; }
+    constexpr Scalar operator-(const Scalar& rhs) const { Scalar retval; retval.data = this->data - rhs.data; return retval; }
+    constexpr Scalar operator/(const Scalar& rhs) const { Scalar retval; retval.data = this->data / rhs.data; return retval; }
+    constexpr Scalar operator*(const Scalar& rhs) const { Scalar retval; retval.data = this->data * rhs.data; return retval; }
+    constexpr Scalar operator+(const T& rhs)      const { Scalar retval; retval.data = this->data + rhs; return retval; }
+    constexpr Scalar operator-(const T& rhs)      const { Scalar retval; retval.data = this->data - rhs; return retval; }
+    constexpr Scalar operator/(const T& rhs)      const { Scalar retval; retval.data = this->data / rhs; return retval; }
+    constexpr Scalar operator*(const T& rhs)      const { Scalar retval; retval.data = this->data * rhs; return retval; }
+
+    constexpr Scalar& operator+=(const Scalar& rhs) { this->data += rhs.data; return *this; }
+    constexpr Scalar& operator-=(const Scalar& rhs) { this->data -= rhs.data; return *this; }
+    constexpr Scalar& operator/=(const Scalar& rhs) { this->data /= rhs.data; return *this; }
+    constexpr Scalar& operator*=(const Scalar& rhs) { this->data *= rhs.data; return *this; }
+    constexpr Scalar& operator+=(const T& rhs)      { this->data += rhs; return *this; }
+    constexpr Scalar& operator-=(const T& rhs)      { this->data -= rhs; return *this; }
+    constexpr Scalar& operator/=(const T& rhs)      { this->data /= rhs; return *this; }
+    constexpr Scalar& operator*=(const T& rhs)      { this->data *= rhs; return *this; }
+
+    constexpr bool operator==(const Scalar& rhs) const { return this->data == rhs.data; }
+    constexpr bool operator!=(const Scalar& rhs) const { return this->data != rhs.data; }
+    constexpr bool operator==(const T& rhs)      const { return this->data == rhs; }
+    constexpr bool operator!=(const T& rhs)      const { return this->data != rhs; }
+
+    public:
+    T data;
+};
+
+// clang-format on
