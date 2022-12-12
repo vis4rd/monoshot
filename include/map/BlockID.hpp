942d834d094ed2e86ea90eb7ab241ff850e397f9
vis4rd <alagaesia.vrael@gmail.com>
2022-12-12 20:53:36 +0100

    ✨ Add invisible wall

--- include/map/BlockID.hpp
+++ include/map/BlockID.hpp
@@ -8,6 +8,7 @@ enum BlockID : std::size_t
 
     // write blocks here
     Wall,
+    InvisibleWall,
 
     LAST_BLOCK,
     BLOCK_COUNT = LAST_BLOCK - FIRST_BLOCK - 1
@@ -18,6 +19,7 @@ inline const char* blockToString(std::size_t block_id)
     switch(block_id)
     {
         case BlockID::Wall: return "Wall"; break;
+        case BlockID::InvisibleWall: return "InvisibleWall"; break;
         default: return "Unknown block"; break;
     }
 }
--- include/map/MapTheme.hpp
+++ include/map/MapTheme.hpp
@@ -18,6 +18,7 @@ struct MapTheme
 
     Color backgroundColor;
     BlockData wallBlock;
+    BlockData invisibleWallBlock;
 };
 
 namespace MapThemes
@@ -26,17 +27,20 @@ namespace MapThemes
 // clang-format off
 static MapTheme TUTORIAL_THEME{
     .backgroundColor = {76.f / 256.f, 55.f / 256.f, 7.f / 256.f, 1.f},  // 76, 55, 7
-    .wallBlock = std::make_tuple(BlockID::Wall, glm::vec4{165.f / 256.f, 146.f / 256.f, 101.f / 256.f, 1.f}, nullptr)  // 165, 146, 101
+    .wallBlock = std::make_tuple(BlockID::Wall, glm::vec4{165.f / 256.f, 146.f / 256.f, 101.f / 256.f, 1.f}, nullptr),  // 165, 146, 101
+    .invisibleWallBlock = std::make_tuple(BlockID::Wall, glm::vec4{0.f, 0.f, 0.f, 0.f}, nullptr),
 };
 
 static MapTheme FOREST_THEME{
     .backgroundColor = {53.f / 256.f, 100.f / 256.f, 53.f / 256.f, 1.f},  // 53, 100, 53
-    .wallBlock = std::make_tuple(BlockID::Wall, glm::vec4{149.f / 256.f, 193.f / 256.f, 149.f / 256.f, 1.f}, nullptr)  // 111, 252, 111
+    .wallBlock = std::make_tuple(BlockID::Wall, glm::vec4{149.f / 256.f, 193.f / 256.f, 149.f / 256.f, 1.f}, nullptr),  // 111, 252, 111
+    .invisibleWallBlock = std::make_tuple(BlockID::Wall, glm::vec4{0.f, 0.f, 0.f, 0.f}, nullptr),
 };
 
 static MapTheme WINTER_THEME{
     .backgroundColor = {215.f / 256.f, 220.f / 256.f, 221.f / 256.f, 1.f},  // 215, 220, 221
-    .wallBlock = std::make_tuple(BlockID::Wall, glm::vec4{0.2f, 0.2f, 0.2f, 1.f}, nullptr)
+    .wallBlock = std::make_tuple(BlockID::Wall, glm::vec4{0.2f, 0.2f, 0.2f, 1.f}, nullptr),
+    .invisibleWallBlock = std::make_tuple(BlockID::Wall, glm::vec4{0.f, 0.f, 0.f, 0.f}, nullptr),
 };
 // clang-format on
 
