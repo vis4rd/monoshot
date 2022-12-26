86429d6999a69dcf95f2ff6a2682505f332fafd6
vis4rd <alagaesia.vrael@gmail.com>
2022-12-26 22:08:15 +0100

    💩 ♻️ Add support for music playback in AudioManager

--- include/audio/AudioManager.hpp
+++ include/audio/AudioManager.hpp
@@ -2,6 +2,7 @@
 
 #include <SFML/Audio/Sound.hpp>
 #include <SFML/Audio/SoundBuffer.hpp>
+#include <SFML/Audio/Music.hpp>
 
 #include <filesystem>
 
@@ -16,15 +17,24 @@ class AudioManager final
 
     bool addSound(const std::string& name, const std::filesystem::path& path, const float& volume = 1.f);
     void playSound(const std::string& name);
+    bool setSoundUserVolume(const std::string& name, const float& new_volume);
+    const float& getSoundUserVolume(const std::string& name) const;
+    float getSoundVolume(const std::string& name) const;
 
-    bool setUserVolume(const std::string& name, const float& new_volume);
-    const float& getUserVolume(const std::string& name) const;
-    float getVolume(const std::string& name) const;
+    bool addMusic(const std::string& name, const std::filesystem::path& path, const float& volume = 1.f);
+    void playMusic(const std::string& name);
+    void pauseMusic(const std::string& name);
+    const std::unique_ptr<sf::Music>& getMusic(const std::string& name);
+    bool setMusicUserVolume(const std::string& name, const float& new_volume);
+    const float& getMusicUserVolume(const std::string& name) const;
+    float getMusicVolume(const std::string& name) const;
 
     private:
     AudioManager() = default;
-    std::size_t getNextFreeIndex() const;
-    bool doesNameExist(const std::string& name) const;
+    std::size_t getNextFreeSoundIndex() const;
+    std::size_t getNextFreeMusicIndex() const;
+    bool doesSoundNameExist(const std::string& name) const;
+    bool doesMusicNameExist(const std::string& name) const;
 
     private:
     std::unordered_map<std::string, std::size_t> m_soundNames;
@@ -32,4 +42,9 @@ class AudioManager final
     std::vector<std::vector<sf::Sound>> m_soundQueues;
     std::vector<float> m_soundInitialVolumes;
     std::vector<float> m_soundUserVolumes;
+
+    std::unordered_map<std::string, std::size_t> m_musicNames;
+    std::vector<std::unique_ptr<sf::Music>> m_musics;
+    std::vector<float> m_musicInitialVolumes;
+    std::vector<float> m_musicUserVolumes;
 };
--- src/audio/AudioManager.cpp
+++ src/audio/AudioManager.cpp
@@ -10,7 +10,7 @@ AudioManager& AudioManager::get()
 
 bool AudioManager::addSound(const std::string& name, const std::filesystem::path& path, const float& volume)
 {
-    if(this->doesNameExist(name))
+    if(this->doesSoundNameExist(name))
     {
         return false;
     }
@@ -19,7 +19,7 @@ bool AudioManager::addSound(const std::string& name, const std::filesystem::path
     bool success = false;
     if(success = buffer.loadFromFile(path); success)
     {
-        m_soundNames[name] = getNextFreeIndex();
+        m_soundNames[name] = getNextFreeSoundIndex();
         m_soundBuffers.push_back(std::move(buffer));
         m_soundQueues.emplace_back();
         m_soundInitialVolumes.emplace_back(volume);
@@ -31,7 +31,7 @@ bool AudioManager::addSound(const std::string& name, const std::filesystem::path
 
 void AudioManager::playSound(const std::string& name)
 {
-    if(not this->doesNameExist(name))
+    if(not this->doesSoundNameExist(name))
     {
         return;
     }
@@ -61,9 +61,9 @@ void AudioManager::playSound(const std::string& name)
     queue.back().play();
 }
 
-bool AudioManager::setUserVolume(const std::string& name, const float& new_volume)
+bool AudioManager::setSoundUserVolume(const std::string& name, const float& new_volume)
 {
-    if(not this->doesNameExist(name))
+    if(not this->doesSoundNameExist(name))
     {
         return false;
     }
@@ -82,13 +82,13 @@ bool AudioManager::setUserVolume(const std::string& name, const float& new_volum
     return true;
 }
 
-const float& AudioManager::getUserVolume(const std::string& name) const
+const float& AudioManager::getSoundUserVolume(const std::string& name) const
 {
     const auto& index = m_soundNames.at(name);
     return m_soundUserVolumes.at(index);
 }
 
-float AudioManager::getVolume(const std::string& name) const
+float AudioManager::getSoundVolume(const std::string& name) const
 {
     const auto& index = m_soundNames.at(name);
     const auto& init_volume = m_soundInitialVolumes.at(index);
@@ -96,12 +96,101 @@ float AudioManager::getVolume(const std::string& name) const
     return 100.f * init_volume * user_volume;
 }
 
-std::size_t AudioManager::getNextFreeIndex() const
+bool AudioManager::addMusic(const std::string& name, const std::filesystem::path& path, const float& volume)
+{
+    if(this->doesMusicNameExist(name))
+    {
+        return false;
+    }
+
+    auto music = std::make_unique<sf::Music>();
+    bool success = false;
+    if(success = music->openFromFile(path); success)
+    {
+        m_musicNames[name] = getNextFreeMusicIndex();
+        auto& init_volume = m_musicInitialVolumes.emplace_back(volume);
+        auto& user_volume = m_musicUserVolumes.emplace_back(1.f);
+        music->setVolume(100.f * init_volume * user_volume);
+        m_musics.push_back(std::move(music));
+    }
+
+    return success;
+}
+
+void AudioManager::playMusic(const std::string& name)
+{
+    if(not this->doesMusicNameExist(name))
+    {
+        return;
+    }
+
+    const auto& index = m_musicNames.at(name);
+    m_musics.at(index)->play();
+}
+
+void AudioManager::pauseMusic(const std::string& name)
+{
+    if(not this->doesMusicNameExist(name))
+    {
+        return;
+    }
+
+    const auto& index = m_musicNames.at(name);
+    m_musics.at(index)->pause();
+}
+
+const std::unique_ptr<sf::Music>& AudioManager::getMusic(const std::string& name)
+{
+    const auto& index = m_musicNames.at(name);
+    return m_musics.at(index);
+}
+
+bool AudioManager::setMusicUserVolume(const std::string& name, const float& new_volume)
+{
+    if(not this->doesMusicNameExist(name))
+    {
+        return false;
+    }
+    auto new_user_volume = std::clamp(new_volume, 0.f, 1.f);
+
+    const auto& index = m_musicNames.at(name);
+    m_musicUserVolumes.at(index) = new_user_volume;
+    const auto& init_volume = m_musicInitialVolumes.at(index);
+    m_musics.at(index)->setVolume(100.f * init_volume * new_user_volume);
+
+    return true;
+}
+
+const float& AudioManager::getMusicUserVolume(const std::string& name) const
+{
+    const auto& index = m_musicNames.at(name);
+    return m_musicUserVolumes.at(index);
+}
+
+float AudioManager::getMusicVolume(const std::string& name) const
+{
+    const auto& index = m_musicNames.at(name);
+    const auto& init_volume = m_musicInitialVolumes.at(index);
+    const auto& user_volume = m_musicUserVolumes.at(index);
+    return 100.f * init_volume * user_volume;
+}
+
+std::size_t AudioManager::getNextFreeSoundIndex() const
 {
     return m_soundBuffers.size();
 }
 
-bool AudioManager::doesNameExist(const std::string& name) const
+std::size_t AudioManager::getNextFreeMusicIndex() const
+{
+    return m_musics.size();
+}
+
+bool AudioManager::doesSoundNameExist(const std::string& name) const
 {
     return m_soundNames.contains(name);
+}
+
+bool AudioManager::doesMusicNameExist(const std::string& name) const
+{
+    return m_musicNames.contains(name);
 }
