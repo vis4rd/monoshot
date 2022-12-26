fa64b12aa0a9aeeb4c52d8adaa61b3759b349794
vis4rd <alagaesia.vrael@gmail.com>
2022-12-26 18:57:21 +0100

    🩹 Fix remaining debug weapon and ammo statistics

--- src/section/TutorialMapSection.cpp
+++ src/section/TutorialMapSection.cpp
@@ -9,6 +9,21 @@ TutorialMapSection::TutorialMapSection()
     m_map.loadFromFile("../res/maps/tutorial.msmap", m_enemyRegistry);
     m_map.addTilesToRegistry(m_mapElementsRegistry);
 
-    m_hero.addItem(std::move(Weapon(10, 31, 76, 35.f, 0.3)));
-    m_hero.addItem(std::move(Weapon(10, 70, 20000, 10.f, 0.05)));
+    {  // rifle
+        constexpr std::int32_t damage = 20;
+        constexpr std::uint32_t magazine_capacity = 31;
+        constexpr std::uint32_t total_ammo = 62;
+        constexpr float bullet_velocity = 35.f;
+        constexpr double use_delay = 0.15;  // in seconds
+        m_hero.addItem(std::move(Weapon(damage, magazine_capacity, total_ammo, bullet_velocity, use_delay)));
+    }
+
+    {  // pistol
+        constexpr std::int32_t damage = 20;
+        constexpr std::uint32_t magazine_capacity = 13;
+        constexpr std::uint32_t total_ammo = 52;
+        constexpr float bullet_velocity = 30.f;
+        constexpr double use_delay = 0.30;  // in seconds
+        m_hero.addItem(std::move(Weapon(damage, magazine_capacity, total_ammo, bullet_velocity, use_delay)));
+    }
 }
