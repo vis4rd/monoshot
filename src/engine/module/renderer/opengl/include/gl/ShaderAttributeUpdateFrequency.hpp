3f597f2c837d677635263cd7f585ee6f4264cfd3
vis4rd <alagaesia.vrael@gmail.com>
2023-12-22 13:56:48 +0100

    📝 Add ShaderAttributeUpdateFrequency documentation

--- src/engine/module/renderer/opengl/include/gl/ShaderAttributeUpdateFrequency.hpp
+++ src/engine/module/renderer/opengl/include/gl/ShaderAttributeUpdateFrequency.hpp
@@ -3,6 +3,10 @@
 namespace mono::gl
 {
 
+/**
+ * @brief Verbose representation of OpenGL divisor for shader attribute. Intended for use with
+ *        VertexArray when passing VertexBuffer to it.
+ */
 enum class ShaderAttributeUpdateFrequency
 {
     EACH_VERTEX = 0,
