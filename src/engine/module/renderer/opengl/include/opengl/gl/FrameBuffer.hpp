83c774ff3a3c939d840d3c331d188608f6186501
vis4rd <alagaesia.vrael@gmail.com>
2024-03-24 01:33:25 +0100

    🎨 Fix method arguments in FrameBuffer class

--- src/engine/module/renderer/opengl/include/opengl/gl/FrameBuffer.hpp
+++ src/engine/module/renderer/opengl/include/opengl/gl/FrameBuffer.hpp
@@ -2,6 +2,7 @@
 
 #include <cstdint>
 
+#include <glad/gl.h>
 #include <glm/glm.hpp>
 
 namespace mono::gl
@@ -10,21 +11,21 @@ namespace mono::gl
 class FrameBuffer
 {
     public:
-    FrameBuffer(const std::int32_t& width, const std::int32_t& height);
+    FrameBuffer(GLsizei width, GLsizei height);
     FrameBuffer(const FrameBuffer&) = delete;
-    FrameBuffer(FrameBuffer&&) = delete;
+    FrameBuffer(FrameBuffer&&) = default;
     ~FrameBuffer();
 
     FrameBuffer& operator=(const FrameBuffer&) = delete;
-    FrameBuffer& operator=(FrameBuffer&&) = delete;
+    FrameBuffer& operator=(FrameBuffer&&) = default;
 
     void bind() const;
     void unbind() const;
-    void resize(const std::int32_t& width, const std::int32_t& height);
+    void resize(GLsizei width, GLsizei height);
 
-    const std::uint32_t& getID() const;
-    const std::uint32_t& getColorID() const;
-    const std::uint32_t& getStencilID() const;
+    GLuint getID() const;
+    GLenum getColorID() const;
+    GLuint getStencilID() const;
     glm::ivec2 getSize() const;
 
     private:
@@ -36,11 +37,11 @@ class FrameBuffer
     void bindAttachments();
 
     private:
-    std::uint32_t m_id = 0u;
-    std::int32_t m_width;
-    std::int32_t m_height;
-    std::uint32_t m_colorAttachment = 0u;
-    std::uint32_t m_stencilAttachment = 0u;
+    GLuint m_id = 0u;
+    GLsizei m_width;
+    GLsizei m_height;
+    GLenum m_colorAttachment = 0u;
+    GLuint m_stencilAttachment = 0u;
 };
 
 }  // namespace mono::gl
