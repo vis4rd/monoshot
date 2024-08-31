dfc6cb32542ec9b1f569ca200309f91d28bf4b24
vis4rd <alagaesia.vrael@gmail.com>
2024-09-01 00:23:53 +0200

    💚 Check if CI caching works

--- src/engine/module/traits/include/traits/ContiguousContainer.hpp
+++ src/engine/module/traits/include/traits/ContiguousContainer.hpp
@@ -8,6 +8,7 @@ namespace mono
 // TODO(vis4rd): Simplify with
 //               https://stackoverflow.com/questions/64228197/range-concept-for-a-specific-type
 
+
 template<typename T, typename E>
 concept ContiguousContainerTrait =
     std::ranges::contiguous_range<T> and std::same_as<typename T::value_type, E> and requires(T t) {
