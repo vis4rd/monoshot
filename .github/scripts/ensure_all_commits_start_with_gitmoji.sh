bc865fac5994a8c1d00f1e60164a82e430987034
vis4rd <alagaesia.vrael@gmail.com>
2024-09-01 02:02:52 +0200

    🔨 Move gitmoji check heavy-lifting from CI workflow to bash script

---
+++ .github/scripts/ensure_all_commits_start_with_gitmoji.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Usage: ./ensure_all_commits_start_with_gitmoji.sh <commit_messages_file> <gitmoji_list_file>
+# Working directory should be the root of the repository.
+
+commit_messages_file=$1
+gitmoji_list_file=$2
+
+# Check each commit message
+is_ok=true
+while IFS= read -r commit; do
+    .scripts/ensure_commit_starts_with_gitmoji.sh "$commit" "$gitmoji_list_file"
+    if [[ $? -eq 1 ]]; then
+        is_ok=false
+        break
+    fi
+done < "$commit_messages_file"
+
+if [[ $is_ok == false ]]; then
+    echo "Commit message does not start with a Gitmoji: '$commit'"
+    exit 1
+fi
