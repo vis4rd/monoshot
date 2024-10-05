afa9f3aad752cb2102dcb79a5ea8287e408ab4d0
vis4rd <alagaesia.vrael@gmail.com>
2024-10-05 19:03:26 +0200

    🔧 Add some window settings to config.ini

--- src/engine/module/config/src/Config.cpp
+++ src/engine/module/config/src/Config.cpp
@@ -31,9 +31,18 @@ bool loadFromFile(const std::filesystem::path& path)
 
 void updateRuntimeConfiguration()
 {
-    config::runtime::useOpenGl = data::iniFile.at("engine").at("UseOpenGL").as<bool>();
+    // engine
+    const auto engine_section = data::iniFile.at("engine");
+    config::runtime::useOpenGl = engine_section.at("UseOpenGL").as<bool>();
     config::runtime::logLevel =
-        spdlog::level_from_str(data::iniFile.at("engine").at("LogLevel").as<std::string>());
+        spdlog::level_from_str(engine_section.at("LogLevel").as<std::string>());
+
+    // engine.window
+    const auto engine_window_section = data::iniFile.at("engine.window");
+    config::runtime::windowMode = engine_window_section.at("Mode").as<type::WindowMode>();
+    config::runtime::useVSync = engine_window_section.at("UseVSync").as<bool>();
+    config::runtime::resolution =
+        engine_window_section.at("Resolution").as<type::WindowResolution>();
 }
 
 void initialize()
