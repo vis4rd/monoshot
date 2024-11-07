69b36c3428c7aed0504656f3fb97a38d167b9b2a
vis4rd <alagaesia.vrael@gmail.com>
2024-11-07 23:16:55 +0100

    ♻️ Implement generic DevUI (part 1)

---
+++ src/engine/module/dev_ui/include/mono/dev_ui/priv/StateData.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+
+namespace mono::dev_ui::priv::state
+{
+
+// NOLINTBEGIN(cppcoreguidelines-avoid-non-const-global-variables)
+std::unordered_map<std::string, bool> window_visibility_flags{};
+// NOLINTEND(cppcoreguidelines-avoid-non-const-global-variables)
+
+}  // namespace mono::dev_ui::priv::state
