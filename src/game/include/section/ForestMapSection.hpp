9b3eeba533993c9ec1a583595ce329523a3c1128
vis4rd <alagaesia.vrael@gmail.com>
2025-02-13 18:26:42 +0100

    🔥 Remove game sources

--- src/game/include/section/ForestMapSection.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "GameplaySection.hpp"
-
-class ForestMapSection final : public GameplaySection
-{
-    public:
-    ForestMapSection();
-};
--- src/game/include/section/WinterMapSection.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "GameplaySection.hpp"
-
-class WinterMapSection final : public GameplaySection
-{
-    public:
-    WinterMapSection();
-};
