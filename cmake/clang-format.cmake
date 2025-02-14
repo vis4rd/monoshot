8968cdc0ad1e2d966e0db90e1318785944629360
vis4rd <alagaesia.vrael@gmail.com>
2025-02-15 00:05:50 +0100

    ⚰️ Remove left-out references to the game

--- cmake/clang-format.cmake
+++ cmake/clang-format.cmake
@@ -4,9 +4,7 @@ if(NOT CLANG_FORMAT_CMD)
     message(FATAL_ERROR "clang-format not found, aborting...")
 endif()
 
-file(GLOB_RECURSE CF_ENGINE_SOURCES "${CMAKE_CURRENT_SOURCE_DIR}/src/engine/module/*.[ch]pp")
-file(GLOB_RECURSE CF_GAME_SOURCES "${CMAKE_CURRENT_SOURCE_DIR}/src/game/*.[ch]pp")
-set(CF_SOURCES "${CF_ENGINE_SOURCES};${CF_GAME_SOURCES}")
+file(GLOB_RECURSE CF_SOURCES "${CMAKE_CURRENT_SOURCE_DIR}/src/engine/module/*.[ch]pp")
 
 message(STATUS "Launching clang-format...")
 execute_process(
