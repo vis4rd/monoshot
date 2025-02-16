f80464b56d64c55db7447a88a95c9ae7cd08ae44
vis4rd <alagaesia.vrael@gmail.com>
2025-02-16 01:25:34 +0100

    🔨 Improve clang-tidy.cmake script

--- cmake/clang-tidy.cmake
+++ cmake/clang-tidy.cmake
@@ -1,39 +1,39 @@
-find_program(CLANG_TIDY_CMD NAMES "clang-tidy")
+include_guard(GLOBAL)
 
-if(NOT CLANG_TIDY_CMD)
-    message(FATAL_ERROR "clang-tidy not found, aborting...")
+# Run clang-tidy on a directory.
+#
+# DIR_PATH: The directory to run clang-tidy on. Every *.cpp, *.hpp and *.inl file will be checked.
+function(run_clang_tidy DIR_PATH)
+    find_program(CLANG_TIDY_CMD NAMES "clang-tidy")
+
+    if(NOT CLANG_TIDY_CMD)
+        message(SEND_ERROR "clang-tidy not found, aborting...")
+        return()
+    endif()
+
+    include("${CMAKE_CURRENT_LIST_DIR}/utils/find-source-files.cmake")
+
+    find_source_files("${DIR_PATH}" CT_SOURCES)
+    list(LENGTH CT_SOURCES CT_SOURCES_COUNT)
+    message(STATUS "There are ${CT_SOURCES_COUNT} files to check.")
+
+    set(CT_FULL_COMMAND ${CLANG_TIDY_CMD} --quiet --config-file=.clang-tidy -p=build/ ${CT_SOURCES})
+    message(STATUS "Launching 1 process of clang-tidy.")
+    message(DEBUG "Working directory: ${CMAKE_CURRENT_SOURCE_DIR}")
+    execute_process(
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+        COMMAND ${CT_FULL_COMMAND}
+    )
+endfunction()
+
+if(CMAKE_SCRIPT_MODE_FILE AND NOT CMAKE_PARENT_LIST_FILE)
+    if(NOT DEFINED CMAKE_ARGV3)
+        message(FATAL_ERROR "DIR_PATH not provided")
+    endif()
+
+    include("${CMAKE_CURRENT_LIST_DIR}/utils/get-cli-args.cmake")
+    get_cli_args(CLI_ARGS)
+    extract_cli_extra_args("${CLI_ARGS}" EXTRA_ARGS)
+
+    run_clang_tidy("${EXTRA_ARGS}")
 endif()
-
-set(ENGINE_SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/src/engine/module")
-file(GLOB_RECURSE CT_SOURCES "${ENGINE_SOURCE_DIR}/*.inl" "${ENGINE_SOURCE_DIR}/*.[ch]pp")
-list(LENGTH CT_SOURCES CT_SOURCES_COUNT)
-message(STATUS "Found ${CT_SOURCES_COUNT} source files to check.")
-
-include("${CMAKE_CURRENT_SOURCE_DIR}/cmake/utils/split_list.cmake")
-
-set(CT_PROCESS_NUM 16)
-split_list(CT_SOURCES ${CT_PROCESS_NUM} CT_SOURCES_CHUNKS)
-list(LENGTH CT_SOURCES_CHUNKS CT_PROCESS_NUM)
-
-set(CT_FULL_COMMAND ${CLANG_TIDY_CMD} --quiet --config-file=.clang-tidy -p=build/)
-
-message(STATUS "Launching ${CT_PROCESS_NUM} processes of clang-tidy.")
-execute_process(
-    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-    COMMAND ${CT_FULL_COMMAND} ${CHUNK_0}
-    COMMAND ${CT_FULL_COMMAND} ${CHUNK_1}
-    COMMAND ${CT_FULL_COMMAND} ${CHUNK_2}
-    COMMAND ${CT_FULL_COMMAND} ${CHUNK_3}
-    COMMAND ${CT_FULL_COMMAND} ${CHUNK_4}
-    COMMAND ${CT_FULL_COMMAND} ${CHUNK_5}
-    COMMAND ${CT_FULL_COMMAND} ${CHUNK_6}
-    COMMAND ${CT_FULL_COMMAND} ${CHUNK_7}
-    COMMAND ${CT_FULL_COMMAND} ${CHUNK_8}
-    COMMAND ${CT_FULL_COMMAND} ${CHUNK_9}
-    COMMAND ${CT_FULL_COMMAND} ${CHUNK_10}
-    COMMAND ${CT_FULL_COMMAND} ${CHUNK_11}
-    COMMAND ${CT_FULL_COMMAND} ${CHUNK_12}
-    COMMAND ${CT_FULL_COMMAND} ${CHUNK_13}
-    COMMAND ${CT_FULL_COMMAND} ${CHUNK_14}
-    COMMAND ${CT_FULL_COMMAND} ${CHUNK_15}
-)
