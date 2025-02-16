500aae311c10bf20cfcc71c8be0c8d8d5e669a1d
vis4rd <alagaesia.vrael@gmail.com>
2025-02-16 01:43:30 +0100

    ⚰️ Remove Renderable, Updateable and Scalar traits

--- src/engine/module/traits/include/traits/Renderable.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-template<typename T>
-concept RenderableTrait = requires(T t) { t.render(); };
--- src/engine/module/traits/include/traits/Updateable.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-template<typename T>
-concept UpdateableTrait = requires(T t) { t.update(); };
