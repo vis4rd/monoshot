3f6844d5ad73e4c1bb4539fe3fac5d7cfb5e43c3
vis4rd <alagaesia.vrael@gmail.com>
2025-10-17 00:14:16 +0200

    🔨 Use run-clang-tidy in scripts, use CMake script in VSCode task

--- cmake/clang-tidy.cmake
+++ cmake/clang-tidy.cmake
@@ -1,39 +1,67 @@
 include_guard(GLOBAL)
 
-# Run clang-tidy on a directory.
+# Helper script for running clang-tidy tool.
 #
-# DIR_PATH: The directory to run clang-tidy on. Every *.cpp, *.hpp and *.inl file will be checked.
-function(run_clang_tidy DIR_PATH)
-    find_program(CLANG_TIDY_CMD NAMES "clang-tidy")
+# Actually, run-clang-tidy tool is used to launch multiple clang-tidy processes in parallel.
+# The number of parallel processes is determined with the following formula:
+# P = N%2 + N/2 + N/8
+# where N is the number of logical processors and P is the number of parallel processes.
+#
+# The above formula is presented in table below for increasing numbers of CPU cores:
+# N | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10| 11| 12| 13| 14| 15| 16| 17| 18|
+# --+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+# P | 1 | 1 | 2 | 2 | 3 | 3 | 4 | 5 | 6 | 6 | 7 | 7 | 8 | 8 | 9 | 10| 11| 11|
+#
+# Additionally, errors/warnings are displayed only from the following paths:
+# - src/*
+# - examples/*
+#
+# Note: It is assumed that this script is run from the root of the workspace.
+# Note: Before running, compile_commands.json must be generated and placed in build/ directory.
+#
+# Sources:
+# - https://clang.llvm.org/extra/doxygen/run-clang-tidy_8py_source.html
+function(launch_clang_tidy)
+    message(STATUS "Hint: Make sure this scipt is run from the root of the workspace.")
 
-    if(NOT CLANG_TIDY_CMD)
-        message(SEND_ERROR "clang-tidy not found, aborting...")
-        return()
+    find_program(CLANG_TIDY_CMD NAMES "clang-tidy" REQUIRED)
+    find_program(RUN_CLANG_TIDY_CMD NAMES "run-clang-tidy" REQUIRED)
+    find_package(Python3 REQUIRED)
+    find_file(COMPILE_COMMANDS_JSON_FILE NAMES "compile_commands.json" PATHS "build/" REQUIRED)
+    find_file(CLANG_TIDY_CONFIG_FILE NAMES ".clang-tidy" PATHS "${CMAKE_CURRENT_SOURCE_DIR}" REQUIRED)
+
+    message(STATUS "Found clang-tidy: ${CLANG_TIDY_CMD}")
+    message(STATUS "Found run-clang-tidy: ${RUN_CLANG_TIDY_CMD}")
+    message(STATUS "Found compile_commands.json: ${COMPILE_COMMANDS_JSON_FILE}")
+    message(STATUS "Found .clang-tidy config file: ${CLANG_TIDY_CONFIG_FILE}")
+
+    # Query number of CPU cores
+    include(ProcessorCount)
+    ProcessorCount(NPROC)
+
+    if(NPROC EQUAL 0)
+        set(NPROC 1)
     endif()
 
-    include("${CMAKE_CURRENT_LIST_DIR}/utils/find-source-files.cmake")
+    # Calculate number of parallel processes
+    math(EXPR NPROC "${NPROC}%2 + ${NPROC}/2 + ${NPROC}/8")
 
-    find_source_files("${DIR_PATH}" CT_SOURCES)
-    list(LENGTH CT_SOURCES CT_SOURCES_COUNT)
-    message(STATUS "There are ${CT_SOURCES_COUNT} files to check.")
+    set(FILTER_BASE_PATH "${CMAKE_CURRENT_SOURCE_DIR}")
+    set(PATH_SEP "/")
 
-    set(CT_FULL_COMMAND ${CLANG_TIDY_CMD} --quiet --config-file=.clang-tidy -p=build/ ${CT_SOURCES})
-    message(STATUS "Launching 1 process of clang-tidy.")
-    message(DEBUG "Working directory: ${CMAKE_CURRENT_SOURCE_DIR}")
+    if(WIN32)
+        set(PATH_SEP "\\\\")
+        string(REPLACE "/" "\\\\" FILTER_BASE_PATH "${FILTER_BASE_PATH}")
+        message(STATUS "Windows detected. Converted base filter path to: ${FILTER_BASE_PATH}")
+    endif()
+
+    set(FILTER "(${FILTER_BASE_PATH}${PATH_SEP}examples|${FILTER_BASE_PATH}${PATH_SEP}src)")
+
     execute_process(
         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-        COMMAND ${CT_FULL_COMMAND}
+        COMMAND_ECHO STDOUT
+        COMMAND ${Python3_EXECUTABLE} ${RUN_CLANG_TIDY_CMD} -config-file ${CLANG_TIDY_CONFIG_FILE} -p build/ -j ${NPROC} ${FILTER}
     )
 endfunction()
 
-if(CMAKE_SCRIPT_MODE_FILE AND NOT CMAKE_PARENT_LIST_FILE)
-    if(NOT DEFINED CMAKE_ARGV3)
-        message(FATAL_ERROR "DIR_PATH not provided")
-    endif()
-
-    include("${CMAKE_CURRENT_LIST_DIR}/utils/get-cli-args.cmake")
-    get_cli_args(CLI_ARGS)
-    extract_cli_extra_args("${CLI_ARGS}" EXTRA_ARGS)
-
-    run_clang_tidy("${EXTRA_ARGS}")
-endif()
+launch_clang_tidy(" ${EXTRA_ARGS} ")
