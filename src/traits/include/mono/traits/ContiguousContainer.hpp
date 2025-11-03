a21de332ecb18efad33891cd60a7fe40c3bc030c
vis4rd <alagaesia.vrael@gmail.com>
2025-11-03 19:57:58 +0100

    🎨 Improve definition of ContiguousContainerTrait

--- src/traits/include/mono/traits/ContiguousContainer.hpp
+++ src/traits/include/mono/traits/ContiguousContainer.hpp
@@ -1,21 +1,21 @@
 #pragma once
 
+#include <concepts>
 #include <ranges>
 
 namespace mono
 {
 
-// TODO(vis4rd): Simplify with
-//               https://stackoverflow.com/questions/64228197/range-concept-for-a-specific-type
-
+// Taken from: https://stackoverflow.com/questions/64228197/range-concept-for-a-specific-type
 
 template<typename T, typename E>
 concept ContiguousContainerTrait =
-    std::ranges::contiguous_range<T> and std::same_as<typename T::value_type, E> and requires(T t) {
-        { t.begin() } -> std::contiguous_iterator;
-        { t[0] } -> std::same_as<E&>;
-        { t.size() } -> std::integral;
-        { t.data() } -> std::same_as<E*>;
-    };
+    std::ranges::contiguous_range<T> and std::same_as<std::ranges::range_value_t<T>, E>;
 
 }  // namespace mono
+
+namespace mono::test
+{
+// static_assert(mono::ContiguousContainerTrait<std::vector<int>, int>);
+// static_assert(not mono::ContiguousContainerTrait<std::vector<int>, float>);
+}  // namespace mono::test
